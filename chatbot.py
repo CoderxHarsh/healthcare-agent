e82c7a72c0e4299e58e365abfb1f4f6869c34c22
Ojaswi ‎ <ojaswi3236@gmail.com>
2026-04-04 12:55:20 +0530

    refined the model's query responses

--- chatbot.py
+++ chatbot.py
@@ -27,14 +27,31 @@ model = ChatGroq(
     temperature =0.7,
     max_tokens = 2048
 )
+#What is temperature in model selection?
+"""0.0 ──────────────────── 1.0 ──────────────────── 2.0
+ │                         │                         │
+ │                         │                         │
+Robotic                 Balanced                  Chaotic
+Always picks          Mix of safe +            Very random,
+safest answer          creative                unpredictable"""
+
+
 def get_response(user_input):
     prompt = f"""
     You are a helpful health assistant.
 
-    Answer clearly and safely.
-    Try to be concise and supportive.
-    Don't give long explanations.`
-    Avoid giving dangerous medical advice.
+    -Answer clearly and safely.
+    -Try to be concise and supportive.
+    -Don't give long explanations.`
+    -Avoid giving dangerous medical advice.
+    -If serious issue → suggest doctor consultation.
+    -Ask follow-up questions if user input is vague.
+    -ask clarifying questions if needed and store the answers in the conversation history.
+    -ask for more details if user input is vague.
+    -Dont start a conversation with new words that define the situation of the user, 
+    always tell about the situation and then use the coined word for it.
+    -If user mentions a health metric (e.g. blood pressure, sugar level, weight),
+    try to extract and save it, but don't ask for it directly.
 
     User: {user_input}
     """
